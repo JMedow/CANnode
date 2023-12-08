314a5d0817da0dd8feea88fc0efc098517720dfc
Jeremy Medow <57373651+JMedow@users.noreply.github.com>
2023-12-08 12:18:12 -0500

    Initial Commit

---
+++ CANnode.cpp
@@ -0,0 +1,474 @@
+/*
+ CANnode.cpp
+ Dec 2023
+
+ Author: Jeremy Medow (jeremy@tungstencustoms.com)
+*/
+
+#include "CANnode.h"
+
+// CANnode Class definitions
+
+// Initialize the MCP_CAN object to talk to the MCP2515 IC, with the correct chip select pin
+CANnode::CANnode(int CS_PIN):_theCAN(CS_PIN){}
+
+// Initialize MCP2515 chip, set filters to listen for this node's address and global messages
+void CANnode::initCAN(uint8_t myID, uint8_t mult = 0){
+
+    _myADDR = myID;
+    _activeADDR = 0;
+
+    while (_theCAN.begin(CAN_SPEED,MCP_8MHz) != CAN_OK){
+        Serial.println("CAN BUS Shield init fail, retrying...");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+
+    if(mult>3)
+    mult = 3;		// Maximum number of multiple addresses, because of filter size
+
+    _numMultADDR = mult;		// Number of additional addresses
+
+    setStandardFilter(mult);		// Listen to the correct messages
+
+}
+
+// If you want to listen to everything on the CAN bus, set your mask and filter to zero
+void CANnode::setNoFilterExt(void){
+    _theCAN.init_Mask(0,1,0);
+    _theCAN.init_Filt(0,1,0);
+}
+
+// Set filters to listen for this node's address and global messages
+void CANnode::setStandardFilter(uint8_t mult = 0){
+    uint32_t MSGMASK_RCV = encodeMSG(0,0xFF,0,0,0);              // Filter on receiver full id
+    uint32_t MSGFILTER_RCV_0 = encodeMSG(0,_myADDR,0,0,0);       // Listen to anyone speaking to _myADDR
+    uint32_t MSGFILTER_RCV_1 = encodeMSG(0,_myADDR+1,0,0,0);			// Listen to anyone speaking to _myADDR + 1
+    uint32_t MSGFILTER_RCV_2 = encodeMSG(0,_myADDR+2,0,0,0);			// ...
+    uint32_t MSGFILTER_RCV_3 = encodeMSG(0,_myADDR+3,0,0,0);			// ...
+
+    uint32_t MSGMASK_SND = encodeMSG(0xFF,0,0,0,0);          // Filter on sender
+    uint32_t MSGFILTER_SND = encodeMSG(BROADCAST_SND,0,0,0,0);  // Listen to anything sent _by_ BROADCAST_SND
+
+    // Universal stuff, as above
+    _theCAN.init_Mask(0,1,MSGMASK_SND);
+    _theCAN.init_Filt(0,1,MSGFILTER_SND);
+    _theCAN.init_Filt(1,1,MSGFILTER_SND);
+
+    // Specific stuff, as above
+    _theCAN.init_Mask(1,1,MSGMASK_RCV);
+    _theCAN.init_Filt(2,1,MSGFILTER_RCV_0);
+    _theCAN.init_Filt(3,1,(mult>0)?MSGFILTER_RCV_1:MSGFILTER_RCV_0);		// Only if there's more than one address
+    _theCAN.init_Filt(4,1,(mult>1)?MSGFILTER_RCV_2:MSGFILTER_RCV_0);		// Only if there's more than two addresses
+    _theCAN.init_Filt(5,1,(mult>2)?MSGFILTER_RCV_3:MSGFILTER_RCV_0);		// Only if there's more than three addresses
+}
+
+// Check the MCP2515 to see whether there is a new message in the buffer
+bool CANnode::msgAvailable(void){
+    return (_theCAN.checkReceive() == CAN_MSGAVAIL);
+}
+
+bool CANnode::updateNode(void){
+  bool toRet = false;
+
+  if(msgAvailable()){
+
+    CANmsg incoming = getComand();
+
+    if(incoming.reg == REG_MULT){
+      switch(incoming.ackRW){
+        case W_ACK:
+          sndAck();
+        case W_NACK:
+          toRet = assignMult(incoming.payload,_lastLen);
+          break;
+        case R_NACK:
+          regRespondAll();
+          break;
+      }
+    } else {
+      switch(incoming.ackRW){
+        case W_ACK:
+          sndAck();
+        case W_NACK:
+          if(_regs[incoming.reg]!=incoming.payload)   // The register will change
+            toRet = true;
+          _regs[incoming.reg] = incoming.payload;   // For write with and without ack
+          break;
+        case R_NACK:
+          regRespond(incoming.reg);
+          break;
+      }
+    }
+
+  return toRet;
+
+}
+
+// Put things in place.  Data is always 7 registers.  Only set mask = 1 registers.
+bool CANnode::assignMult(uint8_t regMask, uint8_t data[]){
+    bool toRet = false;
+
+    for(uint8_t reg = 0; reg < 7; reg++)
+      if(bitRead(regMask,reg)){
+        if(_regs[reg] != data[reg])     // Something will change
+          toRet = true;
+        _regs[reg] = data[reg];
+      }
+
+    return toRet;
+};
+
+// Call this when there is a new message.  It reads the message and dumps the contents into an CANmsg struct and dumps data and len
+CANmsg CANnode::getCommand(void){
+
+    _theCAN.readMsgBuf(&_lastLen,_lastData);		// Pull the data from the MCP2515 chip, put data and len into their variables
+    uint32_t theMSG = _theCAN.getCanId();			// Get the 29-bit id, which contains the addresses and command
+    CANmsg incoming = decodeMSG(theMSG);				// Break apart the id into a CANmsg for parsing
+    setActiveADDR(incoming.rcvID - _myADDR);		// Set the active address (in cases of multiple) based on who was the intended receiver
+
+    _lastSnd = incoming.sndID;						// Keep track of who sent this message for ease of responding
+
+    return incoming;								// Return the CANmsg struct
+}
+
+// Send a message to write to a register
+bool CANnode::regWrite(uint8_t rcvADDR, uint8_t reg, uint8_t payload, bool toAck = false){
+    uint8_t data = 0;
+
+    // Construct the 29-bit id correctly)
+    uint32_t msgID = encodeMSG(_myADDR+_activeADDR,rcvADDR,toAck?W_ACK:W_NACK,reg,payload);
+    // Send the message to the MCP2515 chip, wait a moment to not overload buffers
+    _theCAN.sendMsgBuf(msgID,1,0,data,true);
+    if(TXDELAY)
+      delay(TXDELAY);
+
+    if(toAck){
+      CANmsg incoming;
+
+      uint8_t numTries = 0;
+      uint8_t tempActive = _activeADDR;		// In case of multiple addresses, need to make sure the ACK is going to the right one
+
+      while(numTries < SENDMAX){		// Only try a few times
+
+        uint16_t responsetimer = 0;
+        bool response = false;
+
+        while( (!response) && (responsetimer < ACKTIMEOUT) ){	// Until you timeout
+          response = msgAvailable();
+          if(response){
+            incoming = getCommand();
+  	        // If an incoming message is from the recipient, is an ACK, and is to the correct sender, then success
+            if( (incoming.sndID == rcvADDR) && (incoming.ackRW == ACK) && (_activeADDR == tempActive) )
+              return true;
+            else
+              response = false;
+          }
+          responsetimer+=5;
+          delay(5);
+        }
+
+        numTries++;
+        if(numTries < SENDMAX){       // So you don't send again after the last wait
+          setActiveADDR(tempActive);		// Send as the correct address
+          _theCAN.sendMsgBuf(msgID,1,0,data,true);
+          if(TXDELAY)
+            delay(TXDELAY);
+        }
+
+      }
+
+      return false;   // Didn't get a correct response
+    } else
+      return true;    // If not waiting for an ACK
+}
+
+// Send a message with multiple registers of data
+bool CANnode::regWriteMult(uint8_t rcvADDR, uint8_t regMask, uint8_t payloads[], uint8_t num, toAck = false){
+
+  // Construct the 29-bit id correctly)
+  uint32_t msgID = encodeMSG(_myADDR+_activeADDR,rcvADDR,toAck?W_ACK:W_NACK,REG_MULT,regMask);
+  // Send the message to the MCP2515 chip, wait a moment to not overload buffers
+  _theCAN.sendMsgBuf(msgID,1,7,payloads,true);
+  if(TXDELAY)
+    delay(TXDELAY);
+
+  if(toAck){
+    CANmsg incoming;
+
+    uint8_t numTries = 0;
+    uint8_t tempActive = _activeADDR;		// In case of multiple addresses, need to make sure the ACK is going to the right one
+
+    while(numTries < SENDMAX){		// Only try a few times
+
+      uint16_t responsetimer = 0;
+      bool response = false;
+
+      while( (!response) && (responsetimer < ACKTIMEOUT+(multLen*3)*ACKTIMEOUTDATA) ){	// Until you timeout
+        response = msgAvailable();
+        if(response){
+          incoming = getCommand();
+          // If an incoming message is from the recipient, is an ACK, and is to the correct sender, then success
+          if( (incoming.sndID == rcvADDR) && (incoming.ackRW == ACK) && (_activeADDR == tempActive) )
+            return true;
+          else
+            response = false;
+        }
+        responsetimer+=5;
+        delay(5);
+      }
+
+      numTries++;
+      if(numTries < SENDMAX){       // So you don't send again after the last wait
+        setActiveADDR(tempActive);		// Send as the correct address
+        _theCAN.sendMsgBuf(msgID,1,7,payloads,true);
+        if(TXDELAY)
+          delay(TXDELAY);
+      }
+
+    }
+
+    return false;   // Didn't get a correct response
+  } else
+    return true;    // If not waiting for an ACK
+}
+
+// Return which (of multiple) addresses are active
+uint8_t CANnode::whichADDR(void){
+	return _activeADDR;
+}
+
+// Set the active address, subject to a maximum (user error handling)
+void CANnode::setActiveADDR(uint8_t which){
+	if(which>_numMultADDR)
+		which = _numMultADDR;
+	_activeADDR = which;
+}
+
+// Return the current active address (or the only address if not multiple)
+uint8_t CANnode::myADDR(void){
+    return _myADDR+_activeADDR;
+}
+
+// Fill an array with the data from the last incoming message, and return the length of that array
+uint8_t CANnode::lastData(uint8_t data[]){
+    for(int i = 0; i < _lastLen; i++)
+        data[i] = _lastData[i];
+    return _lastLen;
+}
+
+// Return the value of a register, subject to error handling
+uint8_t CANnode::regVal(uint8_t reg){
+  if(reg<8)
+    return _regs[reg];
+  else
+    return 0xFF;
+}
+
+// Set the value of a register, subject to error handling
+void CANnode::setReg(uint8_t reg, uint8_t val){
+  if(reg < 8)
+    _regs[reg] = val;
+}
+
+// Send ACK to whichever node last sent you a message
+void CANnode::sndACK(void){
+    uint8_t data = 0;
+
+    // Construct the 29-bit id correctly)
+    uint32_t msgID = encodeMSG(_myADDR+_activeADDR,_lastSnd,ACK,0xFF,0xFF);
+    // Send the message to the MCP2515 chip, wait a moment to not overload buffers
+    _theCAN.sendMsgBuf(msgID,1,0,data,true);
+    if(TXDELAY)
+      delay(TXDELAY);
+}
+
+// Respond to the last sender with the contents of the register
+void CANnode::regRespond(uint8_t reg){
+    uint8_t data = 0;
+
+    uint32_t msgID = encodeMSG(_myADDR+_activeADDR,_lastSnd,ACK,reg,_regs[reg]);
+    // Send the message to the MCP2515 chip, wait a moment to not overload buffers
+    _theCAN.sendMsgBuf(msgID,1,0,data,true);
+    if(TXDELAY)
+      delay(TXDELAY);
+}
+
+// Send a READ message a few times, and return the response subject to a timeout
+// Note that other incoming messages during the timeout period will be IGNORED
+uint8_t CANnode::regRead(uint8_t rcvADDR, uint8_t reg){
+  uint8_t data = 0;
+
+  // Construct the 29-bit id correctly)
+  uint32_t msgID = encodeMSG(_myADDR+_activeADDR,rcvADDR,R_NACK,reg,0xFF);
+  // Send the message to the MCP2515 chip, wait a moment to not overload buffers
+  _theCAN.sendMsgBuf(msgID,1,0,data,true);
+  if(TXDELAY)
+    delay(TXDELAY);
+
+  CANmsg incoming;
+
+  uint8_t numTries = 0;
+  uint8_t tempActive = _activeADDR;		// In case of multiple addresses, need to make sure the ACK is going to the right one
+
+  while(numTries < SENDMAX){		// Only try a few times
+
+    uint16_t responsetimer = 0;
+    bool response = false;
+
+    while( (!response) && (responsetimer < ACKTIMEOUT) ){	// Until you timeout
+      response = msgAvailable();
+      if(response){
+        incoming = getCommand();
+        // If an incoming message is from the recipient, is an ACK, and is to the correct sender, then success
+        if( (incoming.sndID == rcvADDR) && (incoming.ackRW == ACK) && (_activeADDR == tempActive) )
+          return incoming.payload;
+        else
+          response = false;
+      }
+      responsetimer+=5;
+      delay(5);
+    }
+
+    numTries++;
+    if(numTries < SENDMAX){       // So you don't send again after the last wait
+      setActiveADDR(tempActive);		// Send as the correct address
+      _theCAN.sendMsgBuf(msgID,1,0,data,true);
+      if(TXDELAY)
+        delay(TXDELAY);
+    }
+
+  }
+
+  return NO_RESPONSE;   // Didn't get a correct response
+
+}
+
+bool regReadAll(uint8_t rcvADDR, uint8_t data[]){
+  uint8_t dummyData = 0;
+
+  // Construct the 29-bit id correctly)
+  uint32_t msgID = encodeMSG(_myADDR+_activeADDR,rcvADDR,R_NACK,REG_MULT,0xFF);
+  // Send the message to the MCP2515 chip, wait a moment to not overload buffers
+  _theCAN.sendMsgBuf(msgID,1,0,dummyData,true);
+  if(TXDELAY)
+    delay(TXDELAY);
+
+  CANmsg incoming;
+
+  uint8_t numTries = 0;
+  uint8_t tempActive = _activeADDR;		// In case of multiple addresses, need to make sure the ACK is going to the right one
+
+  while(numTries < SENDMAX){		// Only try a few times
+
+    uint16_t responsetimer = 0;
+    bool response = false;
+
+    while( (!response) && (responsetimer < ACKTIMEOUT) ){	// Until you timeout
+      response = msgAvailable();
+      if(response){
+        incoming = getCommand();
+        // If an incoming message is from the recipient, is an ACK, and is to the correct sender, then success
+        if( (incoming.sndID == rcvADDR) && (incoming.ackRW == ACK) && (_activeADDR == tempActive) ){
+          for(uint8_t reg = 0; reg < 7; reg++)
+            data[reg] = _lastData[reg];
+          return true;
+        }
+        else
+          response = false;
+      }
+      responsetimer+=5;
+      delay(5);
+    }
+
+    numTries++;
+    if(numTries < SENDMAX){       // So you don't send again after the last wait
+      setActiveADDR(tempActive);		// Send as the correct address
+      _theCAN.sendMsgBuf(msgID,1,0,dummyData,true);
+      if(TXDELAY)
+        delay(TXDELAY);
+    }
+
+  }
+
+  return false;   // Didn't get a correct response
+
+}
+
+void regRespondAll(void){
+    uint32_t msgID = encodeMSG(_myADDR+_activeADDR,_lastSnd,ACK,REG_MULT,0xFF);
+    // Send the message to the MCP2515 chip, wait a moment to not overload buffers
+    _theCAN.sendMsgBuf(msgID,1,7,_regs,true);
+    if(TXDELAY)
+      delay(TXDELAY);
+};
+
+
+// OTHER STUFF -----------------
+
+// Take a 29-bit extended CAN frame id and break it up into component CANnode parts
+CANmsg decodeID(uint32_t theID){
+    CANmsg dec;
+
+    dec.sndID = (theID >> 21) & 0xFF;
+    dec.rcvID = (theID >> 13); & 0xFF
+    dec.ackRW = (theID >> 11) & 0b11;
+    dec.reg = (theID >> 8) & 0b111;
+    dec.payload = theID & 0xFF;
+
+    return dec;
+}
+
+// Basically the opposite of decodeID - take an 11 bit sender and 11 bit receiver id and 7 bit message and pack it into a 29-bit message id
+uint32_t encodeMSG(uint8_t snd_ID, uint8_t rcv_ID, uint8_t ack_rw, uint8_t reg, uint8_t payload){
+  uint8_t maskedReg = reg & 0b111;        // 3 bits only
+  uint8_t maskedAckRW = ack_rw & 0b11;    // 2 bits only
+
+  return ((uint32_t)snd_ID << 21) | ((uint32_t)rcv_id << 13) | ((uint32_t)maskedAckRW << 11) | ((uint32_t)maskedReg << 8) | (uint32_t)payload;
+}
+
+// Take an array of pin numbers, digitalRead() them with pullups enabled, and put the output into a byte
+// Allows grounding pins to set a unique id in hardware, so software can be identical
+uint8_t pollPins(int uniquepins[], int numToRead){
+    uint8_t temp = 0;
+
+    for(int i = 0; i < numToRead; i++){
+        pinMode(uniquepins[i],INPUT_PULLUP);
+        temp |= digitalRead(uniquepins[i])?0:(1<<i);
+    }
+
+    return temp;
+}
+
+// Take an CANmsg and dump its contents to the serial monitor
+/*
+void printMsgHelper(CANmsg theMSG){
+    Serial.print("SND: ");
+    Serial.print(theMSG.sndID,HEX);
+    Serial.print("   RCV: ");
+    Serial.print(theMSG.rcvID,HEX);
+    Serial.print("   RWA: ");
+    switch(theMSG.ackRW){
+      case W_NACK:
+        Serial.print("W");
+        break;
+      case W_ACK:
+        Serial.print("W_ACK");
+        break;
+      case R_NACK:
+        Serial.print("R");
+        break;
+      case ACK:
+        Serial.print("ACK");
+        break;
+    }
+    Serial.print("   Register: ");
+    Serial.print(theMSG.reg);
+    Serial.print("   Payload: ");
+    Serial.println(theMSG.payload);
+
+}/*
+
+/*
+  END FILE
+*/
---
+++ CANnode.h
@@ -0,0 +1,136 @@
+/*
+  CANnode.h
+  Dec 2023
+
+  Author: Jeremy Medow (jeremy@tungstencustoms.com)
+*/
+
+#ifndef CANnode_h
+#define CANnode_h
+
+#include <SPI.h>
+#include <mcp_can.h>
+
+#define CAN_SPEED CAN_40KBPS
+
+#define DEFAULT_CS_PIN 10
+
+#define ELEMENTS(x) (sizeof(x)/sizeof(x[0]))
+
+// For msgSendForACK try SENDMAX times, waiting ACKTIMEOUT ms each time
+#define TXDELAY 5
+#define ACKTIMEOUT 30
+#define ACKTIMEOUTDATA 5
+#define SENDMAX 3
+#define READTIMEOUT 30
+
+#define W_NACK 0b00   // Write, no ACK
+#define W_ACK 0b10    // Write, ACK expected
+#define R_NACK 0b01   // Read, no ACK
+#define ACK 0b11      // ACK
+
+#define REG_STATUS 0
+#define REG_ID 1
+#define REG_TOGGLE 2
+#define REG_SENSOR 3
+#define REG_TRACK 4
+#define REG_PLAY 5
+#define REG_VOL 6
+#define REG_MULT 7
+
+#define NO_RESPONSE 0xFF
+
+#define BROADCAST_SND 0xFF    // Messages sent by this address are heard by everyone
+
+struct CANmsg{
+    uint8_t sndID = 0;
+    uint8_t rcvID = 0;
+    uint8_t ackRW = 0;
+    uint8_t reg = 0;
+    uint8_t payload = 0;
+};
+
+class CANnode{
+public:
+    CANnode(int CS_PIN);
+    // Initializer
+    void initCAN(uint8_t myID, uint8_t mult = 0);
+    // Sets _myADDR, starts CAN communication, sets standard filter - if mult>0, do multiple
+
+    void setNoFilterExt(void);
+    // Listen to all extended frame messages (i.e. everything)
+    void setStandardFilter(uint8_t mult = 0);
+    // Listen to anything addressed to _myADDR and any master-sent 0b11xx messages (RESET, BEAT, etc)
+
+    bool msgAvailable(void);
+    // Return TRUE if a (filtered) message is ready
+    CANmsg getCommand(void);
+    // Returns CANmsg, Dumps data and length into _lastData and _lastLen
+    uint8_t lastData(uint8_t data[]);
+    // Returns the data count of last data sent, fills the data array with the data
+
+    bool updateNode(void);
+    // Checks for messages, and deals with them.  Call repeatedly.  Returns true if any registers change.
+
+    bool regWrite(uint8_t rcvADDR, uint8_t reg, uint8_t payload, bool toAck = false);
+    // Writes the payload to the register of rcvADDR
+    bool regWriteMult(uint8_t rcvADDR, uint8_t regMask, uint8_t payloads[], toAck = false);
+    // Writes the payloads to the registers in the mask of rcvADDR (no ACK)
+    bool assignMult(uint8_t regPayload, uint8_t dataLen);
+    // Puts the data in the right registers, returns true if anything changes
+
+
+    uint8_t myADDR(void);
+    // Return _myADDR
+    uint8_t whichADDR(void);
+    // Which address the last command was sent to
+    void setActiveADDR(uint8_t which);
+    // Set the active (sending) address
+
+    uint8_t regVal(uint8_t reg);
+    // Return the value of a given register
+    void setReg(uint8_t reg, uint8_t val);
+    // Set the value of a given register
+
+    void sndACK(void);
+    // Respond with ACK to the sender of the last message
+
+    uint8_t regRead(uint8_t rcvADDR, uint8_t reg);
+    // Ask for the contents of a register, wait for response (until timeout)
+    void regRespond(uint8_t reg);
+    // Respond to the last sender with the contents of the register
+
+    bool regReadAll(uint8_t rcvADDR, uint8_t data[]);
+    // Puts the contents of all registers into data[]
+    void regRespondAll(void);
+    // Respond with all register content
+
+
+
+private:
+    MCP_CAN _theCAN;
+    uint8_t _myADDR = 0xFF;
+    uint8_t _numMultADDR = 0;
+    uint8_t _activeADDR = 0;
+    uint8_t _lastLen = 0;
+    uint8_t _lastData[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    uint8_t _lastSnd = 0xFF;
+    uint8_t _regs[7] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+};
+
+CANmsg decodeMSG(uint32_t theMSG);
+// Break theMSG into CANmsg struct
+
+uint32_t encodeMSG(uint8_t snd_ID, uint8_t rcv_ID, uint8_t ack_rw, uint8_t reg, uint8_t payload);
+// Shift everything into the right position
+
+uint8_t pollPins(uint8_t uniquepins[], uint8_t numToRead);
+// Poll uniquepins as input pullup to get unique id
+
+//void printMsgHelper(CANmsg theMSG);
+// Print message contents to serial
+
+#endif
+/*
+  END FILE
+*/
